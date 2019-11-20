46e90414c707f8836f3568f44322fb47a8313ce7
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2019-11-20 17:14:56 +0100

    implement Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+//MUI stuff
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <AppBar>
+                <Toolbar className="nav-container">
+                     <Button color="inherit" component={Link} to="/login">
+                       Login
+                     </Button>
+                     <Button color="inherit" component={Link} to="/">
+                        Home
+                     </Button>
+                     <Button color="inherit" component={Link} to="/signup">
+                       Signup
+                     </Button>
+                </Toolbar>
+            </AppBar>
+        )
+    }
+}
