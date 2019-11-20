0bfde24b2cd483533566302102d55559053a70f5
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2019-11-20 17:15:19 +0100

    implement scream component

---
+++ src/components/Scream.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+import withStyles from '@material-ui/core/styles/withStyles';
+import {Link} from 'react-router-dom';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+
+// MUI Stuff
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+    card: {
+        display: 'flex',
+        marginBottom: '20px',
+    },
+    image: {
+        minWidth: '200px',
+    },
+    content: {
+        padding: '25px',
+        objectFit: 'cover'
+    }
+}
+
+class Scream extends Component {
+    render() {
+        dayjs.extend(relativeTime);
+        const { 
+          classes,
+          scream: { 
+              body, 
+              createdAt, 
+              userImage, 
+              userHandle, 
+              screamId, 
+              likeCount, 
+              commentCount
+              } 
+            } = this.props
+        
+        return (
+           <Card className={classes.card}>
+             <CardMedia 
+             image={userImage}  title="Profile image" className={classes.image}/>
+               <CardContent className={classes.content}>
+                  <Typography 
+                  variant="h5"
+                  component={Link}
+                  to={`/users/${userHandle}`}
+                  color="primary"
+                  >
+                  {userHandle}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary">
+                  {dayjs(createdAt).fromNow()}
+                  </Typography> 
+                 <Typography variant="body1">{body}</Typography>
+                 </CardContent>
+           </Card>
+        )
+    }
+}
+
+export default withStyles(styles)(Scream);
