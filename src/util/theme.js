6dfb0aa6ffc9aebd3b807cbf5b962be00b24386b
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2019-11-21 14:01:11 +0100

    put style in an object

--- src/util/theme.js
+++ src/util/theme.js
@@ -13,33 +13,37 @@ export default {
       contrastText: 'fff'
     }
   },
-  typography: {
-    useNextVariants: true
-  },
-  form: {
-        textAlign: 'center'
-    },
-    image: {
-        margin: '20px auto 20px auto'
-    },
-    pageTitle: {
-    margin:  '10px auto 20px auto'
-    },
-    textField: {
-        margin:  '10px auto 20px auto'
-    },
-    button: {
-        marginTop: 20,
-        position: 'relative'
-    }, 
-    customError: {
-        color: 'red',
-        fontSize: '0.8rem',
-        marginTop: 20
-    },
 
-    progress: {
-        position: 'absolute'
-    }
-
+  spreadStyle: {
+    typography: {
+      useNextVariants: true
+    },
+    form: {
+          textAlign: 'center'
+      },
+      image: {
+          margin: '20px auto 20px auto'
+      },
+      pageTitle: {
+      margin:  '10px auto 20px auto'
+      },
+      textField: {
+          margin:  '10px auto 20px auto'
+      },
+      button: {
+          marginTop: 20,
+          position: 'relative'
+      }, 
+      customError: {
+          color: 'red',
+          fontSize: '0.8rem',
+          marginTop: 20
+      },
+  
+      progress: {
+          position: 'absolute'
+      }
+  
+  }
+  
 }
