9255ba42efb0c69db1f73ce816ad31d2a5bb42cd
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2019-11-22 11:59:52 +0100

    setup userActions in redux

---
+++ src/redux/actions/userActions.js
@@ -0,0 +1,67 @@
+import axios from 'axios';
+import {SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI, SET_UNAUTHENTICATED } from '../types';
+
+
+export const loginUser = (userData, history) => (dispatch) => {
+    dispatch({ type: LOADING_UI });
+    axios
+    .post('/login', userData)
+    .then((res) => {
+        setAuthorizationHeader(res.data.token)
+        dispatch(getUserData());
+        dispatch({ type: CLEAR_ERRORS})
+        history.push('/');
+       })
+    .catch(err => {
+        dispatch({
+            type: SET_ERRORS, 
+            payload: err.response.data
+        })
+    })
+}
+
+
+export const signupUser = (newUserData, history) => (dispatch) => {
+    dispatch({ type: LOADING_UI });
+    
+    axios
+    .post('signup', newUserData)
+    .then((res) => {
+        setAuthorizationHeader(res.data.token)
+        dispatch(getUserData());
+        dispatch({ type: CLEAR_ERRORS})
+        history.push('/');
+       })
+    .catch(err => {
+        dispatch({
+            type: SET_ERRORS, 
+            payload: err.response.data
+        })
+    })
+}
+
+export const logoutUser = () => (dispatch) => {
+    localStorage.removeItem('FBIdToken');
+    delete axios.defaults.headers.common['Authorization'];
+    dispatch({ type: SET_UNAUTHENTICATED });
+}
+
+export const getUserData = () => (dispatch) => {
+axios.get('/user')
+     .then(res => {
+         dispatch({
+             type: SET_USER,
+             payload: res.data
+         })
+     })
+     .catch(err => console.log(err));
+}
+
+
+const setAuthorizationHeader = (token) => {
+    
+    const FBIdToken = `Bearer ${token}`;
+        localStorage.setItem('FBIdToken', FBIdToken);
+        axios.defaults.headers.common['Authorization'] = FBIdToken;
+
+}
