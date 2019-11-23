4fe6eaaf1157318f51b88f1d669c2214d6c7fc38
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2019-11-23 20:42:20 +0100

    import profile

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import Grid from '@material-ui/core/Grid';
 
 import Scream from '../components/Scream';
+import Profile from '../components/Profile';
 
 class home extends Component {
     state = {
@@ -26,12 +27,12 @@ class home extends Component {
             ) : <p>Loading...</p>
  
         return (
-           <Grid container spacing={10}>
+           <Grid container spacing={2}>
                <Grid item sm={8} xs={12}>
                   {recentScreamsMarkup}
                 </Grid>
                 <Grid item sm={4} xs={12}>
-                  <p>Profile...</p>
+                  <Profile />
                 </Grid>
             </Grid>
         )
