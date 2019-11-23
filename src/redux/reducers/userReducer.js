6ef9b511b1559f3e28c01fbef7bddc844ca6d5f6
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2019-11-23 20:44:35 +0100

    add action loading_user

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,7 +1,8 @@
-import {SET_AUTHENTICATED, SET_UNAUTHENTICATED, SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_Ui } from '../types';
+import {SET_USER, SET_AUTHENTICATED, SET_UNAUTHENTICATED, LOADING_USER } from '../types';
 
 const initialState = {
     authenticated: false,
+    loading: false,
     credentials: {},
     likes: [],
     notifications: []
@@ -19,8 +20,14 @@ export default function(state = initialState, action){
             case SET_USER:
                 return {
                     authenticated: true,
+                    loading: false,
                     ...action.payload
                 };
+            case LOADING_USER:
+                return {
+                    ...state,
+                    loading: true
+                }
                 default:
                 return state;
     }
