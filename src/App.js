45a51673f0a4e481e7cc088bee980cfa3f8f2cb5
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2019-11-23 20:41:30 +0100

    connect to redux

--- src/App.js
+++ src/App.js
@@ -8,9 +8,8 @@ import jwtDecode from 'jwt-decode';
 // Redux
 import { Provider } from 'react-redux';
 import store from './redux/store';
-
-
-
+import { SET_AUTHENTICATED } from './redux/types';
+import { logoutUser, getUserData } from './redux/actions/userActions';
 
 //components
 import Navbar from './components/Navbar';
@@ -20,21 +19,22 @@ import AuthRoute from './util/AuthRoute';
 import home from './pages/home';
 import signup from './pages/signup';
 import login from './pages/login';
+import axios from 'axios';
 
 
 const theme = createMuiTheme(themeFile);
 
-let authenticated;
+
 const token = localStorage.FBIdToken;
 if(token){
  const decodedToken = jwtDecode(token);
- console.log(decodedToken);
- console.log(Date.now());
  if(decodedToken.exp * 1000 < Date.now()){
+   store.dispatch(logoutUser())
    window.location.href = '/login';
-   authenticated = false
  } else {
-   authenticated = true;
+   store.dispatch({ type: SET_AUTHENTICATED })
+   axios.defaults.headers.common['Authorization'] = token;
+   store.dispatch(getUserData())
  }
 }
 
@@ -49,8 +49,8 @@ class App extends Component {
           <div className="container">
              <Switch>
                <Route exact path="/" component={home} />
-               <AuthRoute exact path="/login" component={login} authenticated={authenticated}/>
-               <AuthRoute exact path="/signup" component={signup} authenticated={authenticated}/>
+               <AuthRoute exact path="/login" component={login} />
+               <AuthRoute exact path="/signup" component={signup} />
              </Switch>
           </div>
          </Router>
