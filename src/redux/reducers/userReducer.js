9d445ccfe331d69263c9e4f23b7e8971632b77a2
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2019-11-25 10:03:11 +0100

    add case likescream and unlike scream

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,4 +1,4 @@
-import {SET_USER, SET_AUTHENTICATED, SET_UNAUTHENTICATED, LOADING_USER } from '../types';
+import {SET_USER, SET_AUTHENTICATED, SET_UNAUTHENTICATED, LOADING_USER, LIKE_SCREAM, UNLIKE_SCREAM } from '../types';
 
 const initialState = {
     authenticated: false,
@@ -28,6 +28,22 @@ export default function(state = initialState, action){
                     ...state,
                     loading: true
                 }
+            case LIKE_SCREAM:
+                return {
+                    ...state,
+                    likes: [
+                        ...state.likes,
+                        {
+                            userHandle: state.credentials.handle,
+                            screamId: action.payload.screamId
+                        }
+                    ]
+                }
+                case UNLIKE_SCREAM:
+                    return {
+                        ...state,
+                        likes: state.likes.filter(like => like.screamId !== action.payload.screamId)
+                    }
                 default:
                 return state;
     }
