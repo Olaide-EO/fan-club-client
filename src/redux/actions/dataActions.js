5fd15cdd2d40d0018a63f3ab3fd41d18cfa4b1d2
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2019-11-25 10:00:48 +0100

    impement data actions

---
+++ src/redux/actions/dataActions.js
@@ -0,0 +1,86 @@
+import {
+    SET_SCREAMS, 
+    LOADING_DATA, 
+    LIKE_SCREAM, 
+    UNLIKE_SCREAM, 
+    DELETE_SCREAM,
+    SET_ERRORS,
+    POST_SCREAM,
+    CLEAR_ERRORS,
+    LOADING_UI
+ } from '../types';
+import axios from 'axios';
+
+// Get all screams
+export const getScreams = () => dispatch => {
+    dispatch({ type: LOADING_DATA });
+    axios.get('/scream')
+       .then(res => {
+           dispatch({
+               type: SET_SCREAMS,
+               payload: res.data
+           })
+       })
+       .catch(err => {
+           dispatch({
+               type: SET_SCREAMS,
+               payload: []
+           })
+       })
+}
+
+// Post a scream
+export const postScream = (newScream) => (dispatch) => {
+    dispatch({ type: LOADING_UI });
+    axios.post('/scream', newScream)
+    .then(res => {
+        dispatch({
+            type: POST_SCREAM,
+            payload: res.data
+        });
+        dispatch({ type: CLEAR_ERRORS })
+    })
+    .catch( err => {
+        dispatch({
+            type: SET_ERRORS,
+            payload: err.response.data
+        })
+    })
+}
+
+
+// Like a scream
+export const likeScream = (screamId) => dispatch => {
+    axios.get(`/scream/${screamId}/like`)
+       .then(res => {
+           dispatch({
+               type: LIKE_SCREAM,
+               payload: res.data
+           })
+       })
+       .catch(err => console.log(err));
+}
+
+export const unlikeScream = (screamId) => dispatch => {
+    axios.get(`/scream/${screamId}/unlike`)
+       .then(res => {
+           dispatch({
+               type: UNLIKE_SCREAM,
+               payload: res.data
+           })
+       })
+       .catch(err => console.log(err));
+}
+
+export const deleteScream = (screamId) => (dispatch) => {
+    axios.delete(`/scream/${screamId}`)
+       .then(() => {
+           dispatch({ type: DELETE_SCREAM, payload: screamId })
+       })
+       .catch(err => console.log(err))
+}
+
+
+
+
+
