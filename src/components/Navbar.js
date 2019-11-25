a790b481c1cb00f7104d265ff7d8d72e34736cb8
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2019-11-25 09:55:41 +0100

    implement postascrem functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import {Link} from 'react-router-dom';
+import { connect }  from 'react-redux';
+import PropTypes from 'prop-types';
+import MyButton from '../util/MyButton';
+import PostScream from './PostScream';
 //MUI stuff
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
+// Icons
+import HomeIcon from '@material-ui/icons/Home';
+import Notifications from '@material-ui/icons/Notifications';
 
 
-export default class Navbar extends Component {
+
+class Navbar extends Component {
     render() {
+      const { authenticated } = this.props
         return (
             <AppBar>
                 <Toolbar className="nav-container">
-                     <Button color="inherit" component={Link} to="/login">
+                    {authenticated ? (
+                     
+                     <Fragment>
+                      <PostScream/>
+                      <Link to="/">
+                         <MyButton tip="Home">
+                          <HomeIcon />
+                         </MyButton>
+                      </Link>
+                       <MyButton tip="Notifications">
+                        <Notifications />
+                       </MyButton>
+                     </Fragment>
+
+                    ) : (
+                      <Fragment>
+                      <Button color="inherit" component={Link} to="/login">
                        Login
                      </Button>
                      <Button color="inherit" component={Link} to="/">
@@ -20,8 +45,20 @@ export default class Navbar extends Component {
                      <Button color="inherit" component={Link} to="/signup">
                        Signup
                      </Button>
+                      </Fragment>
+                    )}
                 </Toolbar>
             </AppBar>
         )
     }
 }
+
+Navbar.propTypes = {
+  authenticated: PropTypes.bool.isRequired
+}
+
+const mapStateToProps = state => ({
+  authenticated: state.user.authenticated
+})
+
+export default connect(mapStateToProps)(Navbar)
