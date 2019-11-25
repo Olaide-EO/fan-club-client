552fd681887f23776935153bf1d7ba3597084a39
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2019-11-25 10:02:03 +0100

    initial setup of data reducer

---
+++ src/redux/reducers/dataReducer.js
@@ -0,0 +1,51 @@
+import { SET_SCREAMS, LIKE_SCREAM, UNLIKE_SCREAM, LOADING_DATA, DELETE_SCREAM, POST_SCREAM } from '../types';
+
+const initialState = {
+    screams: [],
+    scream: {},
+    loading: false
+}
+
+export default function(state = initialState, action){
+    switch(action.type){
+        case LOADING_DATA:
+            return {
+                ...state,
+                loading: true
+            };
+        case SET_SCREAMS:
+            return {
+                ...state,
+                screams: action.payload,
+                loading: false
+            }
+        case LIKE_SCREAM:
+        case UNLIKE_SCREAM:
+            let index = state.screams.findIndex(
+                (scream) => scream.screamId === action.payload.screamId
+                );
+            state.screams[index] = action.payload;
+            return {
+                ...state
+            };
+        case DELETE_SCREAM:
+            index = state.screams.findIndex(
+                (scream) => scream.screamId === action.payload
+                );
+            state.screams.splice(index, 1);
+            return {
+                ...state
+            }
+        case POST_SCREAM:
+            return {
+                ...state,
+                screams: [
+                    action.payload,
+                    ...state.screams
+                ]
+            }
+         default: 
+           return state;   
+        }
+         
+}
