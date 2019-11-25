6a942d5a79f51459693024bd59a75656554ddf33
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2019-11-25 09:59:35 +0100

    connect redux store

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,30 +1,27 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Grid from '@material-ui/core/Grid';
-
+import PropTypes from 'prop-types';
 import Scream from '../components/Scream';
 import Profile from '../components/Profile';
 
+import { connect } from 'react-redux';
+import { getScreams } from '../redux/actions/dataActions';
 class home extends Component {
-    state = {
-        screams: null
-    }
+    
     componentDidMount(){
-    axios.get('/scream')
-         .then(res => {
-           this.setState({
-               screams: res.data
-           })
-         })
-         .catch(err => console.log(err));
-
+    this.props.getScreams(); 
     }
 
 
     render() {
-          let recentScreamsMarkup = this.state.screams ? (
-         this.state.screams.map((scream) => <Scream key={scream.screamId} scream={scream}/>)
-            ) : <p>Loading...</p>
+        const { screams, loading } = this.props.data
+          let recentScreamsMarkup = !loading ? (
+         screams.map((scream) => 
+         <Scream key={scream.screamId} scream={scream}/>)
+            ) : 
+            <p>Loading...</p>
+            ;
  
         return (
            <Grid container spacing={2}>
@@ -39,4 +36,13 @@ class home extends Component {
     }
 }
 
-export default home;
+home.propTypes = {
+    getScreams: PropTypes.func.isRequired,
+    data: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+    data: state.data
+})
+
+export default connect(mapStateToProps, { getScreams })(home);
