59ab3345d2498fd951b6d01701c07e786b8d1205
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2019-11-25 09:56:49 +0100

    initial setup of postasccream

---
+++ src/components/PostScream.js
@@ -0,0 +1,106 @@
+import React, { Component, Fragment } from 'react'
+import PropTypes from 'prop-types';
+import withStyles from '@material-ui/core/styles/withStyles';
+import MyButton from '../util/MyButton';
+
+//MUI Stuff
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import AddIcon from '@material-ui/icons/Add';
+import CloseIcon from '@material-ui/icons/Close';
+
+// Redux stuff
+import { connect } from 'react-redux';
+import { postScream } from '../redux/actions/dataActions';
+
+
+const styles = theme => ({
+    ...theme.spreadStyle,
+    submitButton: {
+        position: 'relative'
+    },
+    progressSpinner: {
+        position: 'absolute'
+    },
+    closeButton: {
+        position: 'absolute',
+        left: '90%',
+        top: '10%'
+    }
+
+})
+
+class PostScream extends Component{
+   state = {
+       open: false,
+       body: '',
+       errors: {}
+   };
+   handleOpen = () => {
+       this.setState({ open: true })
+   }
+   handleClose = () => {
+    this.setState({ open: false })
+}
+   render(){
+       const { errors } = this.state;
+       const { classes, UI: { loading }} = this.props;
+       return (
+           <Fragment>
+             <MyButton onClick={this.handleOpen} tip="Post a Scream!">
+               <AddIcon />
+             </MyButton>
+             <Dialog
+               open={this.state.open}
+               onClose={this.handleClose}
+               fullWidth
+               maxWidth="sm">
+                 <MyButton tip="Close" onClick={this.handleClose} tipClassName={classes.closeButton}>
+                  <CloseIcon/>
+                 </MyButton>
+                 <DialogTitle>Post a new scream</DialogTitle>
+                 <DialogContent>
+                  <form onSubmit={this.hanleSubmit}>
+                     <TextField
+                       name="body"
+                       type="text"
+                         label="SCREAM!!"
+                         multiline
+                         rows="3"
+                         placeholder="Scream at your fellow apes"
+                         error={errors.body ? true: false}
+                         helperText={errors.body}
+                         className={classes.textField}
+                         onChange={this.handleChange}
+                         fullWidth
+                         />
+                         <Button type="submit" variant="contained" color="primary"
+                         className={classes.submitButton}
+                         disabled={loading}>
+                         Submit
+                         {loading && (
+                            <CircularProgress size={30} className={classes.progressSpinner}/>
+                         )}
+                         </Button>
+                  </form>
+                 </DialogContent>
+               </Dialog>
+              
+           </Fragment>
+       )
+   }
+}
+
+PostScream.propTypes = {
+    postScream: PropTypes.func.isRequired,
+    UI: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+    UI: state.UI
+})
+export default connect(mapStateToProps, { postScream})(withStyles(styles)(PostScream))
