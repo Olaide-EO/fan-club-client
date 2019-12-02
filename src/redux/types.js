3085a9f8d38c2014d88a5278e72d9ceed2d3a13c
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2019-12-02 15:48:09 +0100

    add marknotificationsread dand submitcomment

--- src/redux/types.js
+++ src/redux/types.js
@@ -3,6 +3,7 @@ export const SET_AUTHENTICATED = 'SET_AUTHENTICATED';
 export const SET_UNAUTHENTICATED = 'SET_UNAUTHENTICATED';
 export const SET_USER = 'SET_USER';
 export const LOADING_USER = 'LOADING_USER';
+export const MARK_NOTIFICATIONS_READ = 'MARK_NOTIFICATIONS_READ';
 // UI reducer types
 export const SET_ERRORS = 'SET_ERRORS';
 export const LOADING_UI = 'LOADING_UI';
@@ -16,4 +17,5 @@ export const SET_SCREAM = 'SET_SCREAM';
 export const LIKE_SCREAM = 'LIKE_SCREAM';
 export const UNLIKE_SCREAM = 'UNLIKE_SCREAM';
 export const DELETE_SCREAM = 'DELETE_SCREAM';
-export const POST_SCREAM = 'POST_SCREAM' 
+export const POST_SCREAM = 'POST_SCREAM';
+export const SUBMIT_COMMENT = 'SUBMIT_COMMENT'; 
