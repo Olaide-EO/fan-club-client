15f9ab144cb861c55f16b8b91bab99dd38949e49
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2019-12-02 15:51:55 +0100

    add marknotifcationsread reducer

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,4 +1,4 @@
-import {SET_USER, SET_AUTHENTICATED, SET_UNAUTHENTICATED, LOADING_USER, LIKE_SCREAM, UNLIKE_SCREAM } from '../types';
+import {SET_USER, SET_AUTHENTICATED, SET_UNAUTHENTICATED, LOADING_USER, LIKE_SCREAM, UNLIKE_SCREAM, MARK_NOTIFICATIONS_READ } from '../types';
 
 const initialState = {
     authenticated: false,
@@ -43,6 +43,11 @@ export default function(state = initialState, action){
                     return {
                         ...state,
                         likes: state.likes.filter(like => like.screamId !== action.payload.screamId)
+                    }
+                case MARK_NOTIFICATIONS_READ:
+                    state.notifications.forEach((not) => (not.read = true));
+                    return {
+                        ...state
                     }
                 default:
                 return state;
