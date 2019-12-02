3ab3c4470bd55afb91ca1d45238305ab0c299d8e
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2019-12-02 15:49:39 +0100

    implement getuserdata and submitcomment

--- src/redux/actions/dataActions.js
+++ src/redux/actions/dataActions.js
@@ -9,7 +9,8 @@ import {
     CLEAR_ERRORS,
     LOADING_UI,
     SET_SCREAM,
-    STOP_LOADING_UI
+    STOP_LOADING_UI,
+    SUBMIT_COMMENT
  } from '../types';
 import axios from 'axios';
 
@@ -53,7 +54,7 @@ export const postScream = (newScream) => (dispatch) => {
             type: POST_SCREAM,
             payload: res.data
         });
-        dispatch({ type: CLEAR_ERRORS })
+        dispatch(clearErrors());
     })
     .catch( err => {
         dispatch({
@@ -86,6 +87,25 @@ export const unlikeScream = (screamId) => dispatch => {
        })
        .catch(err => console.log(err));
 }
+// Submit a comment
+export const submitComment = (screamId, commentData) => (dispatch) => {
+    axios.post(`/scream/${screamId}/comment`, commentData)
+       .then(res => {
+           dispatch({
+               type: SUBMIT_COMMENT,
+               payload: res.data
+           });
+           dispatch(clearErrors())
+        })
+        .catch(err => {
+            dispatch({
+                type: SET_ERRORS,
+                payload: err.response.data
+            })
+        })
+}
+
+
 
 export const deleteScream = (screamId) => (dispatch) => {
     axios.delete(`/scream/${screamId}`)
@@ -94,6 +114,24 @@ export const deleteScream = (screamId) => (dispatch) => {
        })
        .catch(err => console.log(err))
 }
+
+
+export const getUserData = (userHandle) => dispatch => {
+    dispatch({ type: LOADING_DATA });
+    axios.get(`/user/${userHandle}`)
+       .then(res => {
+           dispatch({
+               type: SET_SCREAMS,
+               payload: res.data.screams
+           });
+       })
+       .catch(() => {
+           dispatch({
+               type: SET_SCREAMS,
+               payload: null
+           })
+       })
+}  
 
 export const clearErrors = () => dispatch => {
     dispatch({ type: CLEAR_ERRORS })
