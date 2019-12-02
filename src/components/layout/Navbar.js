94b2686cd423081c136e0ef892df9884b99b0a8b
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2019-12-02 15:36:46 +0100

    ad notifications icon

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,6 +4,7 @@ import { connect }  from 'react-redux';
 import PropTypes from 'prop-types';
 import MyButton from '../../util/MyButton';
 import PostScream from '../scream/PostScream';
+import Notifications from './Notifications';
 
 //MUI stuff
 import AppBar from '@material-ui/core/AppBar';
@@ -11,7 +12,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 // Icons
 import HomeIcon from '@material-ui/icons/Home';
-import Notifications from '@material-ui/icons/Notifications';
+
 
 
 
@@ -30,9 +31,9 @@ class Navbar extends Component {
                           <HomeIcon />
                          </MyButton>
                       </Link>
-                       <MyButton tip="Notifications">
+                      
                         <Notifications />
-                       </MyButton>
+                       
                      </Fragment>
 
                     ) : (
