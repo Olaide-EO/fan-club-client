c69352cb29ec22b5928387b53a270d0f12431002
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2019-12-02 15:43:57 +0100

    add openDialog function

--- src/components/scream/Scream.js
+++ src/components/scream/Scream.js
@@ -86,7 +86,7 @@ class Scream extends Component {
                    <ChatIcon color="primary"/>
                  </MyButton>
                  <span>{commentCount} comments </span>
-                 <ScreamDialog screamId={screamId} userHandle={userHandle}/>
+                 <ScreamDialog screamId={screamId} userHandle={userHandle} openDialog={this.props.openDialog}/>
                  </CardContent>
            </Card>
         )
@@ -96,7 +96,8 @@ class Scream extends Component {
 Scream.propTypes = {
     user: PropTypes.object.isRequired,
     scream: PropTypes.object.isRequired,
-    classes: PropTypes.object.isRequired
+    classes: PropTypes.object.isRequired,
+    openDialog: PropTypes.bool
 }
 
 const mapStateToProps = state => ({
