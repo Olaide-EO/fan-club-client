e4f7dc02dfa09e9996e322557dfd07efe7fd2d98
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2020-03-30 09:36:43 +0100

    remove redux dev tools for production

--- src/redux/store.js
+++ src/redux/store.js
@@ -18,7 +18,7 @@ const reducers = combineReducers({
 
 const store = createStore(reducers, initialState, compose(
     applyMiddleware(...middleware), 
-    window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()
+   
     )
     )
 
